ee363aed6638f11afafc3be322dd72d9814dd6aa
ebgr <ebgr@g3c44.cin.ufpe.br>
2024-12-04 09:35:28 -0300

    feat: inserido requisicao que retorna cobrancas não expiradas.

--- src/main/java/com/ebgr/pagamento_carnes/repository/PaymentRepository.java
+++ src/main/java/com/ebgr/pagamento_carnes/repository/PaymentRepository.java
@@ -4,6 +4,7 @@ import com.ebgr.pagamento_carnes.model.PaymentModel;
 import com.ebgr.pagamento_carnes.model.UserModel;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,5 +12,10 @@ public interface PaymentRepository  extends CrudRepository<PaymentModel, Integer
     public Optional<PaymentModel> findByUserAndPaymentMonthAndPaymentYear(UserModel user, int payment_month, int payment_year);
     public List<PaymentModel> findByUser(UserModel user);
 
+    //List<PaymentModel> findAllByUserAndExpiresAtAfter(UserModel user, LocalDateTime expiresAt);
+    List<PaymentModel> findAllByUserAndExpiresAtAfterOrExpiresAtIsNull(UserModel user, LocalDateTime expiresAt);
+    List<PaymentModel> findAllByExpiresAtBefore(LocalDateTime expiresAt);
+
     public List<PaymentModel> findAll();
+    //public void delete(List<PaymentModel> paymentModels);
 }
